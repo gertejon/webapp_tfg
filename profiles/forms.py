c8258fa325a23e1eaa1c61ee06d655c79cdc9f58
gertejon <1526000@uab.cat>
2023-05-06 20:30:34 +0200

    update forms

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    image = forms.ImageField(help_text='Profile Picture')
+    #image = forms.ImageField(help_text='Profile Picture')
 
 
     class Meta:
